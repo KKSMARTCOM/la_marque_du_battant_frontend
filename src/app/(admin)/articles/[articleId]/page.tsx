2972dcce809fc8efd9421a0242615425a0fec6c5
Arso991 <arsogn991@gmail.com>
2024-07-05 17:13:36 +0100

    update [articleId]/page

--- src/app/(admin)/articles/[articleId]/page.tsx
+++ src/app/(admin)/articles/[articleId]/page.tsx
@@ -4,11 +4,21 @@ import ProductForm from "@/components/admin/articles/ProductForm";
 import Loader from "@/components/custom ui/Loader";
 import React, { useState } from "react";
 
-export default function ProductDetails() {
+export default function ProductDetails({
+  params,
+}: {
+  params: { articleId: string };
+}) {
   const [loading, setLoading] = useState(true);
   const [productDetails, setProductDetails] = useState<ProductType | null>(
     null
   );
 
   return loading ? <Loader /> : <ProductForm initialData={productDetails} />;
+}
+
+export function generateStaticParams() {
+  return productsData.map((item) => {
+    item._id;
+  });
 }
